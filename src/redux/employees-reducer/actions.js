a31b06a78b497ddff8e393e842ab24d5872bd393
Antony343 <example@example.com>
2021-07-09 20:12:17 +0200

    final commit

--- src/redux/employees-reducer/actions.js
+++ src/redux/employees-reducer/actions.js
@@ -2,12 +2,12 @@ import { remasterEmployeesArrayStructure } from "../../utils/utils";
 import { setEmployees } from "./actionCreators";
 import { employeesService } from "./api.service";
 
-export const fetchEmployees = () => (dispatch) => {
-  employeesService
-    .getEmployees()
-    .then((resp) => {
-      const restructured = remasterEmployeesArrayStructure(resp.data);
-      dispatch(setEmployees(restructured))}
-      )
-    .catch((err) => console.error(err));
+export const fetchEmployees = () => async (dispatch) => {
+  try {
+    let resp = await employeesService.getEmployees();
+    const restructured = remasterEmployeesArrayStructure(resp.data);
+    dispatch(setEmployees(restructured))
+  } catch (error) {
+    return Promise.reject(error)
+  }
 };
